7a3269dcfdd7dc0abf48f9f1f005cd3b37e80231
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-26 12:07:24 +0100

    Add recipeInput class and style

---
+++ helloworld/src/RecipeInput.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './RecipeInput.css'; 
+import RecipeList from './RecipeList';
+
+class RecipeInput extends Component{
+constructor(props){
+  super(props);
+}
+
+} 
+
+export default RecipeInput;
